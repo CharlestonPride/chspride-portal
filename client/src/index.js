092f1261588650473052f24602079a9b5752289c
Pete Bingel <bingelp@gmail.com>
2020-02-08 21:12:14 -0500

    inital argon dashboard

---
+++ client/src/index.js
@@ -0,0 +1,63 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faChartBar,
+  faArrowUp,
+  faChartPie,
+  faArrowDown,
+  faUsers,
+  faPercent,
+  faSearch,
+  faEllipsisV,
+  faAngleLeft,
+  faAngleRight
+} from "@fortawesome/free-solid-svg-icons";
+
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/scss/argon-dashboard-react.scss";
+
+import AdminLayout from "layouts/Admin.jsx";
+import AuthLayout from "layouts/Auth.jsx";
+
+library.add(
+  faChartBar,
+  faArrowUp,
+  faChartPie,
+  faArrowDown,
+  faUsers,
+  faPercent,
+  faSearch,
+  faEllipsisV,
+  faAngleLeft,
+  faAngleRight
+);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/admin" render={props => <AdminLayout {...props} />} />
+      <Route path="/auth" render={props => <AuthLayout {...props} />} />
+      <Redirect from="/" to="/admin/index" />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
